b2cc984033a210c57749906aecf5d78ef671d774
Thomas Morris <tmorris@covermymeds.com>
2018-01-08 05:45:25 -0500

    BUGFIX - Make ComicVote.upvote persist data to DB

--- spec/use_cases/up_vote_spec.rb
+++ spec/use_cases/up_vote_spec.rb
@@ -3,16 +3,32 @@ require 'rails_helper'
 RSpec.describe FetchComics do
 
   context 'when a vote is given' do
-    before do
-      ComicVote.create(comic_id: 123)
+    let(:comic_id) { 123 }
+
+    context 'and a ComicVote does NOT exist' do
+      before { UpVote.perform(comic_id) }
+
+      it 'creates a ComicVote object with votes == 1' do
+        comic_votes = ComicVote.find_by(comic_id: comic_id)
+        expect(comic_votes).not_to be_nil
+        expect(comic_votes.votes).to eq 1
+      end
     end
 
-    it 'increases votes amount if ComicVote exists for the comic_id' do
-      UpVote.perform(123)
+    context 'and a ComicVote already exists' do
+      before do
+        ComicVote.create(comic_id: comic_id)
+      end
 
-      comic_votes = ComicVote.find_by(comic_id: 123)
+      it 'does not create any new ComicVote objects' do
+        expect{ UpVote.perform(comic_id) }.not_to change { ComicVote.count }
+      end
 
-      expect(comic_votes.votes).not_to be_nil
+      it 'increments votes on the existing object' do
+        expect{ UpVote.perform(comic_id) }
+          .to change { ComicVote.find_by(comic_id: comic_id).votes }
+          .by(1)
+      end
     end
   end
 end
